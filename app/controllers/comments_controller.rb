7e6f619246d8a5b3a6ba9214f89bae519b859a33
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2022-05-21 17:39:11 +0900

    コメントを保存したのち コメントしたツイートの詳細画面へリダイレクト

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,8 @@
 class CommentsController < ApplicationController
 
   def create
-    Comment.create(comment_params)
+    comment = Comment.create(comment_params)
+    redirect_to tweet_path(comment.tweet.id)
   end
 
   private
